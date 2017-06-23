edf6c2742ef551d37cd9456be6ae8da0c5267edd
Jon Goetz <jongoetz@users.noreply.github.com>
2017-06-23 15:59:54 -0700

    Script for analyzing a flow dataset for various summary stats.

--- FASST-R_flow_analysis.R
+++ FASST-R_flow_analysis.R
@@ -21,7 +21,7 @@
 ### Version:      23 June 2017
 ### Source:       https://github.com/bcgov/FASST
 
-### Contact:      Jon Goetz (BC ENV) at jon.goetz@gov.bc.ca
+### Contact:      Jon Goetz (BC ENV) https://github.com/jongoetz
 
 
 ## Description:   This script summarizes continuous daily stream discharge into various long-term, annual, monthly, and daily stream
